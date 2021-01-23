49c22ceec536bc09061ac9b27bb1792ae9621c8c
Rahul Pande <rahulxpande@gmail.com>
2021-01-23 22:18:31 +0530

    New paragraph and heading

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,8 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Rahul Pande</h1>
+    <p>A Brand + Experience Designer based in India; creating Identities, websites & mobile apps. Currently working at Adobe.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
